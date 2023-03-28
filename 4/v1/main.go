8fce016fa17aac482629179c3941d1df716a8126
Artem <nilovtema@yandex.ru>
2023-03-28 10:39:20 +0300

    Remove dangerous code

--- 4/v1/main.go
+++ 4/v1/main.go
@@ -1,5 +1,13 @@
 package main
 
+/*
+Реализовать постоянную запись данных в канал (главный поток).
+Реализовать набор из N воркеров, которые читают
+произвольные данные из канала и выводят в stdout.
+Необходима возможность выбора количества воркеров при старте.
+Программа должна завершаться по нажатию Ctrl+C.
+Выбрать и обосновать способ завершения работы всех воркеров.
+*/
 import (
 	"flag"
 	"fmt"
@@ -12,36 +20,35 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// Реализовать постоянную запись данных в канал (главный поток).
-// Реализовать набор из N воркеров, которые читают
-// произвольные данные из канала и выводят в stdout.
-// Необходима возможность выбора количества воркеров при старте.
-// Программа должна завершаться по нажатию Ctrl+C.
-// Выбрать и обосновать способ завершения работы всех воркеров.
-
 func main() {
-	//Создаем
 	var wg sync.WaitGroup
-	//разобраться во всех параметрах
+	//Устанавливаем флаг
 	numWorkers := flag.Int("N", 1, "Number of workers")
 	flag.Parse()
 	fmt.Printf("Number of workers %v\n", *numWorkers)
+	//Создаем канал для прерываний
 	interrupt := make(chan os.Signal, 1)
+	//Создаем канал, в который мы отправим горутинам сигнал о завершении работы
+	exit := make(chan struct{}, 1)
+	//Создаем канал для значений
 	values := make(chan int, 1000)
-	// signal.Notify registers the given channel to
-	// receive notifications of the specified signals.
+	// signal.Notify регистрирует данный канал для
+	// получений уведомлений об определенных сигналах от ОС
 	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+	//Запускаем горутины
 	for i := 1; i <= *numWorkers; i++ {
 		wg.Add(1)
-		go worker(values, &wg, i)
+		go worker(exit, values, &wg, i)
 	}
 	for {
 		select {
 		case <-interrupt:
 			{
 				logrus.Errorln("Received SIGINT, quiting")
+				//exit <- struct{}{} - не требуется, так как можно просто закрыть канал
+				close(exit)
 				close(values)
-				defer wg.Wait()
+				wg.Wait()
 				return
 			}
 		default:
@@ -52,13 +59,22 @@ func main() {
 	}
 }
 
-func worker(values <-chan int, wg *sync.WaitGroup, id int) {
+func worker(exit <-chan struct{}, values <-chan int, wg *sync.WaitGroup, id int) {
 	for value := range values {
-		logrus.Infof("Value %v was received by worker %v\n", value, id)
-
-		//когда небуферизованный канал закрыт,
-		//то прога вылетает и часть с Exit не срабатывает
+		//С помощью неблокирующего оператора select читаем два канал
+		//Как только придет значение в exit, горутина завершится
+		select {
+		case <-values:
+			{
+				logrus.Infof("Value %v was received by worker %v\n", value, id)
+			}
+		case <-exit:
+			{
+				//Делаем любые операции необходимые для завершения горутины
+				logrus.Warnf("Worker %v is done", id)
+				wg.Done()
+				return
+			}
+		}
 	}
-	logrus.Warnf("Worker %v is done", id)
-	wg.Done()
 }
